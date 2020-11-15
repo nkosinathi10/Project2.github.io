01f3da259fb3fa455b94ed405cb1cf9d483a3d61
Nkosinathi Khanyile <nkosinathikhanyile10@gmail.com>
2020-11-16 01:00:44 +0200

    Setting up roles and Database

---
+++ OrganisationX/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OrganisationX.Data;
+
+[assembly: HostingStartup(typeof(OrganisationX.Areas.Identity.IdentityHostingStartup))]
+namespace OrganisationX.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ OrganisationX/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrganisationX.Controllers
+{
+    public class UserController : Controller
+    {
+        [Authorize(Roles="User")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
